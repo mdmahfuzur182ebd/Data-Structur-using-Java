689b60b78682ee509ba9f4dd8d2d0b0f4fbb4f72
Mahfuzur <mdmahfuzur980@gmail.com>
2021-10-21 12:22:32 +0600

    something added

--- src/HelloJava/searchFile.java
+++ src/HelloJava/searchFile.java
@@ -5,7 +5,7 @@ class Search{
     void search(int key){
         for (int i=0; i<arr.length; i++) {
             if (arr[i] == key) {
-                System.out.println(i);
+                System.out.println("Index Number:"+i);
 
             }
         }
